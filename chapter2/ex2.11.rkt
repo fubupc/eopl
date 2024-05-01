2cbd402a956008df118988afa88f055cebceb30a
Yao Fu <fubupc@gmail.com>
2024-05-01 13:58:14 +0800

    Exercise 2.11: fix bug and refactor

--- chapter2/ex2.11.rkt
+++ chapter2/ex2.11.rkt
@@ -1,6 +1,5 @@
 #lang racket
 
-(require eopl)
 
 ; Exercise 2.11 [★★] A naive implementation of `extend-env*` from the preceding exercise requires
 ; time proportional to k to run. It is possible to represent environments so that `extend-env*`
@@ -13,8 +12,25 @@
 ;
 ; Implement the environment interface, including `extend-env*`, in this representation.
 
-; Env = () | (Rib . Env)
-; Rib = (Listof(Var) . Listof(SchemeVal))
+(require eopl)
+
+; Env = () | Listof(RibPair)
+; RibPair = (Listof(Var) . Listof(SchemeVal))
+
+; rib-pair : Listof(Var) × Listof(SchemeVal) → RibPair
+(define rib-pair (lambda (vars vals) (cons vars vals)))
+
+; rib-pair->vars : RibPair → Listof(Var)
+(define rib-pair->vars (lambda (rib) (car rib)))
+
+; rib-pair->vals : RibPair → Listof(SchemeVal)
+(define rib-pair->vals (lambda (rib) (cdr rib)))
+
+; rib-pair->add : Var × SchemeVal × RibPair → RibPair
+(define rib-pair->add
+  (lambda (var val rib)
+    (rib-pair (cons var (rib-pair->vars rib))
+              (cons val (rib-pair->vals rib)))))
 
 ; empty-env : () → Env
 (define empty-env (lambda () '()))
@@ -23,13 +39,10 @@
 (define extend-env
   (lambda (var val env)
     (if (null? env)
-        (cons (list (list var) (list val)) '())
+        (list (rib-pair (list var) (list val)))
         (let ((first-rib (car env))
               (rest-env (cdr env)))
-          (cons
-           (list (cons var (car first-rib))
-                 (cons val (cadr first-rib)))
-           rest-env)))))
+          (cons (rib-pair->add var val) rest-env)))))
 
 ; apply-env : Env × Var → SchemeVal
 (define apply-env
@@ -38,14 +51,17 @@
         (report-no-binding-found search-var)
         (apply-env-in-rib (car env) (cdr env) search-var))))
 
-; apply-env-in-rib : Listof(Var) × Listof(SchemeVal) × Env × Var → SchemeVal
+; apply-env-in-rib : RibPair × Env × Var → SchemeVal
 (define apply-env-in-rib
   (lambda (first-rib rest-env search-var)
-    (let ((vars (car first-rib))
-          (vals (cadr first-rib)))
+    (let ((vars (rib-pair->vars first-rib))
+          (vals (rib-pair->vals first-rib)))
       (cond ((null? vars) (apply-env rest-env search-var))
             ((eqv? (car vars) search-var) (car vals))
-            (else (apply-env-in-rib (cdr vars) (cdr vals) search-var))))))
+            (else (apply-env-in-rib
+                   (rib-pair (cdr vars) (cdr vals))
+                   rest-env
+                   search-var))))))
 
 (define report-no-binding-found
   (lambda (search-var)
@@ -54,6 +70,14 @@
 ; extend-env* : Listof(Var) × Listof(SchemeVal) × Env → Env
 (define extend-env*
   (lambda (vars vals env)
-    (cons (list vars vals) env)))
+    (cons (rib-pair vars vals) env)))
 
-(apply-env (extend-env* '(a b c) '(1 2 3) (empty-env)) 'a)
+(module+ test
+  (require rackunit)
+
+  (define e (extend-env* '(c d) '(4 5) (extend-env* '(a b c) '(1 2 3) (empty-env))))
+  (check-equal? (apply-env e 'a) 1)
+  (check-equal? (apply-env e 'b) 2)
+  (check-equal? (apply-env e 'c) 4) ; not 3
+  (check-equal? (apply-env e 'd) 5)
+  (check-exn exn:fail? (lambda () (apply-env e 'e))))
